1b505a6ac728304e2b58f2ab27b32ab66710c91b
soyouda <soyouda@gmail.com>
2020-06-04 16:35:00 +0900

    1.2 Setting Up the Server like the Pros

--- src/schema.js
+++ src/schema.js
@@ -3,11 +3,12 @@ import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 
 /** api 폴더 내에는 .graphql, .js 이외의 파일이 있을 시 오류 발생함! */
-const allTypes = fileLoader(path.join(__dirname,"/api/**/*.graphql"));
-const allResolvers =  fileLoader(path.join(__dirname, "/api/**/*.js"));
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
+const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
 
 const schema = makeExecutableSchema({
-    typeDefs: mergeTypes(allTypes),
-    resolver: mergeResolvers(allResolvers)
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers)
 });
+
 export default schema;
--- src/server.js
+++ src/server.js
@@ -1,26 +1,14 @@
-require("dotenv").config()
+require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan"; // logger(로깅 전용 모듈)
-
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String
-    }
-`;
-
-const resolvers = {
-    Query: {
-        hello: () => "Hi"
-    }
-}
-
-const server = new GraphQLServer({typeDefs, resolvers})
+const server = new GraphQLServer({ schema });
 
 server.express.use(logger("dev"));
 
-server.start({ port: PORT }, () => console.log(`Server running on port http://localhost:${PORT}`));
-
-
+server.start({ port: PORT }, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
+);
