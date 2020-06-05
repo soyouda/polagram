8a89283ea56a99cfb72554702469537575a9d333
soyouda <soyouda@gmail.com>
2020-06-05 23:57:09 +0900

    using prisma context

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,8 +1,6 @@
-import { prisma } from "../../../../generated/prisma-client";
-
 export default {
   Mutation: {
-    createAccount: async (_, args) => {
+    createAccount: async (_, args, { prisma }) => {
       const { name, email, firstName = "", lastName = "", bio = "" } = args;
       const user = await prisma.createUser({
         name,
